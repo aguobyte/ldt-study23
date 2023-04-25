70877b19b41fa75f75f838273e1ceecd6be08a8d
aguobyte <75400863+aguobyte@users.noreply.github.com>
2023-04-25 13:36:13 +0200

    Add files via upload

---
+++ ldt_qualtrics.js
@@ -0,0 +1,87 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualtrics/qamp.js'], function(APIConstructor, ampExtension){
+
+	var API = new APIConstructor();
+	
+	
+	return ampExtension({
+		primeCats :  [
+			{
+				nameForFeedback : 'Conducive Exercise',  //Will be used in the user feedback 
+				nameForLogging : 'Conducive Exercise', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [ 
+					{image : 'exercise_ball.jpg'}, 
+					{image : 'kettlebell.jpg'}, 
+					{image : 'protein_shake.jpg'}, 
+					{image : 'treadmill.jpg'}, 
+					{image : 'weights.jpg'},
+					{image : 'yoga_mat.jpg'},
+					{image : 'dumbells1.jpg'}]
+
+			}, 
+			{
+				nameForFeedback : 'Non-Conducive Exercise',  //Will be used in the user feedback 
+				nameForLogging : 'Non-Conducive Exercise', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {image : 'armchair.jpg'}, 
+					{image : 'bathtub.jpg'}, 
+					{image : 'bed.jpg'}, 
+					{image : 'couch.jpg'},
+					{image : 'hammock.jpg'},
+					{image : 'tv.jpg'},
+					{image : 'remote_control.jpg'}]
+			}, 
+			{
+				nameForFeedback : 'Conducive Food',  //Will be used in the user feedback 
+				nameForLogging : 'Conducive Food', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {image : 'apple.jpg'}, 
+					{image : 'berries.jpg'}, 
+					{image : 'Broccoli.jpg'}, 
+					{image : 'carrots.jpg'},
+					{image : 'eggs.jpg'},
+					{image : 'nuts.jpg'},
+					{image : 'tomato.jpg'}]
+			},
+			{
+				nameForFeedback : 'Non-Conducive Food',  //Will be used in the user feedback 
+				nameForLogging : 'Non-Conducive Food', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {image : 'waffels.jpg'}, 
+					{image : 'pizza.jpg'}, 
+					{image : 'donuts.jpg'}, 
+					{image : 'brownie.jpg'},
+					{image : 'hamburger.jpg'},
+					{image : 'ice_cream.jpg'},
+					{image : 'chocolate.jpg'}]
+			},
+			{
+				nameForFeedback : 'Neutral',  //Will be used in the user feedback 
+				nameForLogging : 'Neutral', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {image : 'book2.jpg'}, 
+					{image : 'calculator.jpg'}, 
+					{image : 'pencil_sharpner.jpg'}, 
+					{image : 'folders.jpg'},
+					{image : 'keyboard.jpg'},
+					{image : 'notebook.jpg'},
+					{image : 'Pen.jpg'}]
+			}
+		],
+
+		examplePrimeStimulus : 
+		{
+			nameForLogging : 'examplePrime', //Will be used in the logging
+			//An array of all media objects for this category.
+			mediaArray : [{image : 'ampchair.jpg'}, {image : 'amplamp.jpg'}, {image : 'ampumbrella.jpg'}]
+		},
+
+		base_url : {//Where are your images at?
+			image : 'https://aguobyte.github.io/vid-con-amp/images/'
+		}
+	});
+});
